55af76ed49d84e155a6d1db3687ac83346772cbe
Marius Matulevicius <marius.matulevicius@twoday.com>
2024-09-16 18:01:18 +0300

    task 2 tests

--- taskTests/html_css/tests.js
+++ taskTests/html_css/tests.js
@@ -5,6 +5,24 @@ const task1 = () => {
   };
 }
 
+const task2 = () => {
+  const result = {
+    aTagExists: false,
+    hrefAttributeExists: false,
+    hrefIsGoogle: false,
+  };
+  try {
+    const aTag = document.querySelector("a");
+    result.aTagExists = !!aTag;
+    result.hrefAttributeExists = aTag.href !== "";
+    result.hrefIsGoogle = aTag.href.includes("https://www.google.com") || aTag.href.includes("https://google.com") ;
+  } catch (error) {
+    console.log(error);
+  }
+  return result;
+}
+
 export default {
   ["362abec3-81b2-4361-870c-a4e054781d73"] : task1,
+  ["46ae62bc-0db1-4e99-94af-d52d3f2cdfd5"] : task2,
 };
